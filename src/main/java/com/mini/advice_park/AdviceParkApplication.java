9f2549c285ef62d9efbb7686aa20d9cf3105044e
Minhee Moon <minhee610@gmail.com>
2024-02-23 15:28:00 +0900

    init commit

---
+++ src/main/java/com/mini/advice_park/AdviceParkApplication.java
@@ -0,0 +1,13 @@
+package com.mini.advice_park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdviceParkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdviceParkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mini/advice_park/AdviceParkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mini.advice_park;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdviceParkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
