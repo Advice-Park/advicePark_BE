6d7c1dded808e74090825299202b922e886707de
Minhee Moon <minhee610@gmail.com>
2024-02-26 00:43:13 +0900

    Update: 헬스체크 수정

--- src/main/java/com/mini/advice_park/controller/HealthCheckController.java
+++ src/main/java/com/mini/advice_park/controller/HealthCheckController.java
@@ -1,15 +1,17 @@
 package com.mini.advice_park.controller;
 
 import com.mini.advice_park.dto.exception.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api")
 public class HealthCheckController {
-    @GetMapping("/health")
-    public ResponseDto<?> healthCheck() {
-        return ResponseDto.ok("health check");
+    @GetMapping("/health-check")
+    public ResponseEntity<Void> checkHealthStatus() {
+
+        return new ResponseEntity<>(HttpStatus.OK);
     }
 }
