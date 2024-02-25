c4332c7fd36a4380bb63ff97f0bbce3264aaad36
Minhee Moon <minhee610@gmail.com>
2024-02-26 02:59:17 +0900

    Update: 헬스체크 수정

--- src/main/java/com/mini/advice_park/controller/HealthCheckController.java
+++ src/main/java/com/mini/advice_park/controller/HealthCheckController.java
@@ -1,10 +1,8 @@
 package com.mini.advice_park.controller;
 
-import com.mini.advice_park.dto.exception.ResponseDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
