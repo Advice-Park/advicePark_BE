a0a225ace6781abcbefabb1753cc5d78aa6fcd1c
Minhee Moon <minhee610@gmail.com>
2024-02-26 15:35:12 +0900

    Feat: 구글로그인 전체 수정

---
+++ src/main/java/com/mini/advice_park/exception/OAuth2AuthenticationProcessingException.java
@@ -0,0 +1,9 @@
+package com.mini.advice_park.exception;
+
+import org.springframework.security.core.AuthenticationException;
+
+public class OAuth2AuthenticationProcessingException extends AuthenticationException {
+    public OAuth2AuthenticationProcessingException(String msg) {
+        super(msg);
+    }
+}
---
+++ src/main/java/com/mini/advice_park/oauth2/user/OAuth2UserUnlink.java
@@ -0,0 +1,5 @@
+package com.mini.advice_park.oauth2.user;
+
+public interface OAuth2UserUnlink {
+    void unlink(String accessToken);
+}
