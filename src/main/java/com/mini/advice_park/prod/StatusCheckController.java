46dd4b05f9bcd8a590d313a415d0ea73b3661a04
Minhee Moon <minhee610@gmail.com>
2024-02-29 10:16:04 +0900

    Update: health check endpoint

--- src/main/java/com/mini/advice_park/prod/StatusCheckController.java
+++ src/main/java/com/mini/advice_park/prod/StatusCheckController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class StatusCheckController {
 
-    @GetMapping("/")
+    @GetMapping("/health-check")
     public ResponseEntity<Void> checkHealthStatus() {
 
         return new ResponseEntity<>(HttpStatus.OK);
