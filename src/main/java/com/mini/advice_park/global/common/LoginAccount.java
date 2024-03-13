05e2a30e9e28b60a6e6b35067373cb41ad2a3391
Minhee Moon <minhee610@gmail.com>
2024-03-13 17:52:41 +0900

    Update: 로그인확인 로직 변경

--- src/main/java/com/mini/advice_park/global/common/LoginAccount.java
+++ src/main/java/com/mini/advice_park/global/common/LoginAccount.java
@@ -9,6 +9,6 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
-@AuthenticationPrincipal(expression = "user")
+@AuthenticationPrincipal(expression = "username")
 public @interface LoginAccount {
 }
