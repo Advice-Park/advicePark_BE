c89ced3cabd8d7a54b4b69fe3cc0a628e1877d1c
Minhee Moon <minhee610@gmail.com>
2024-03-16 14:27:25 +0900

    Update: 글작성시 생성시간 반환추가

--- src/main/java/com/mini/advice_park/domain/post/dto/PostResponse.java
+++ src/main/java/com/mini/advice_park/domain/post/dto/PostResponse.java
@@ -6,6 +6,7 @@ import com.mini.advice_park.domain.post.entity.VoteOption;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -25,6 +26,8 @@ public class PostResponse {
     private final long viewCount;
     private final long commentCount;
 
+    private LocalDateTime createdTime;
+
     private final List<String> imageUrls;
 
     @Builder
@@ -36,6 +39,7 @@ public class PostResponse {
                         VoteOption voteOption,
                         long viewCount,
                         long commentCount, // 댓글 수 추가 필드
+                        LocalDateTime createdTime,
                         List<String> imageUrls) {
 
         this.postId = postId;
@@ -46,6 +50,7 @@ public class PostResponse {
         this.voteOption = voteOption;
         this.viewCount = viewCount;
         this.commentCount = commentCount; // 댓글 수 필드 초기화
+        this.createdTime = createdTime;
         this.imageUrls = imageUrls != null ? imageUrls : Collections.emptyList(); // 이미지 URL 리스트가 null일 경우 빈 리스트로 초기화
 
     }
@@ -66,6 +71,7 @@ public class PostResponse {
                 .voteOption(post.getVoteOption())
                 .viewCount(post.getViewCount())
                 .commentCount(post.getCommentCount()) // 댓글 수 추가
+                .createdTime(post.getCreatedTime())
                 .imageUrls(imageUrls)
                 .build();
     }
