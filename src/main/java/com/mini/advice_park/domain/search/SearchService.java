d1d140597c502e7cce7581d848d87dcf3a20ce0d
Minhee Moon <minhee610@gmail.com>
2024-03-20 20:41:17 +0900

    Update: 제목, 내용 통합해서 검색

--- src/main/java/com/mini/advice_park/domain/search/SearchService.java
+++ src/main/java/com/mini/advice_park/domain/search/SearchService.java
@@ -22,22 +22,22 @@ public class SearchService {
      */
     @Transactional(readOnly = true)
     public List<PostResponse> searchPosts(String keyword) {
-
-        // 제목과 내용에 대한 검색을 수행하여 모든 결과를 가져옴
+        // 제목에 대한 검색 결과 가져오기
         List<PostResponse> postsByTitle = postRepository.findByTitleContaining(keyword).stream()
-                .map(PostResponse::from) // PostResponse로 변환
+                .map(PostResponse::from)
                 .collect(Collectors.toList());
 
+        // 내용에 대한 검색 결과 가져오기
         List<PostResponse> postsByContents = postRepository.findByContentsContaining(keyword).stream()
-                .map(PostResponse::from) // PostResponse로 변환
+                .map(PostResponse::from)
                 .collect(Collectors.toList());
 
-        // 두 결과를 조합하여 하나의 리스트로 반환하거나,
-        // 필요에 따라 서로 다른 방식으로 합치거나 처리할 수 있음
-
-        return postsByTitle.stream()
-                .distinct() // 중복 제거
+        // 두 결과를 조합하여 하나의 리스트로 반환
+        List<PostResponse> combinedResults = Stream.concat(postsByTitle.stream(), postsByContents.stream())
+                .distinct()
                 .collect(Collectors.toList());
+
+        return combinedResults;
     }
 
 }
