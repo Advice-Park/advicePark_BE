abbdeec0fdfbd0a493475d0e8e16bea52485de33
Minhee Moon <minhee610@gmail.com>
2024-04-06 15:39:20 +0900

    Update: 마이페이지 즐겨찾기 응답값 수정

--- src/main/java/com/mini/advice_park/domain/favorite/dto/UserPostFavoriteDto.java
+++ src/main/java/com/mini/advice_park/domain/favorite/dto/UserPostFavoriteDto.java
@@ -1,6 +1,7 @@
 package com.mini.advice_park.domain.favorite.dto;
 
 import com.mini.advice_park.domain.favorite.entity.UserPostFavorite;
+import com.mini.advice_park.domain.post.dto.PostResponse;
 import lombok.Getter;
 
 @Getter
@@ -8,19 +9,19 @@ public class UserPostFavoriteDto {
 
     private Long favoriteId;
     private Long userId;
-    private Long postId;
+    private PostResponse post; // 포스트의 전체 내용을 담을 객체
 
-    public UserPostFavoriteDto(Long favoriteId, Long userId, Long postId) {
+    public UserPostFavoriteDto(Long favoriteId, Long userId, PostResponse post) {
         this.favoriteId = favoriteId;
         this.userId = userId;
-        this.postId = postId;
+        this.post = post;
     }
 
     public static UserPostFavoriteDto from(UserPostFavorite userPostFavorite) {
         return new UserPostFavoriteDto(
                 userPostFavorite.getFavoriteId(),
                 userPostFavorite.getUser().getUserId(),
-                userPostFavorite.getPost().getPostId()
+                PostResponse.from(userPostFavorite.getPost())
         );
     }
 
