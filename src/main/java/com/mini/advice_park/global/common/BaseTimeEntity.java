890887d5abb7fb6518be39e43ecc2cd834512d25
Minhee Moon <minhee610@gmail.com>
2024-04-27 15:27:24 +0900

    Update: ZonedDateTime으로 수정

--- src/main/java/com/mini/advice_park/global/common/BaseTimeEntity.java
+++ src/main/java/com/mini/advice_park/global/common/BaseTimeEntity.java
@@ -8,6 +8,7 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 
 @Getter
 @MappedSuperclass
@@ -15,9 +16,9 @@ import java.time.LocalDateTime;
 public abstract class BaseTimeEntity {
 
     @CreatedDate
-    private LocalDateTime createdTime;
+    private ZonedDateTime createdTime;
 
     @LastModifiedDate
-    private LocalDateTime lastModifiedTime;
+    private ZonedDateTime lastModifiedTime;
 
 }
